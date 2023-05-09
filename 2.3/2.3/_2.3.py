4dbce8e9a94832b87b725d97dcbc1cf993a92209
Edgeeee11 <arsenykurenkov@gmail.com>
2023-05-09 18:56:42 +0300

    Homework

--- 2.3/2.3/_2.3.py
+++ 2.3/2.3/_2.3.py
@@ -16,12 +16,24 @@ else:
 #Задание 2
 
 word = input("Введите слово: ")
+a_count = 0 
+e_count = 0 
+i_count = 0 
+o_count = 0 
+u_count = 0 
+y_count = 0
 
-vowels = "aeiou"
+vowels = "aeiouy"
 vowel_count = 0
 consonant_count = 0
 
 for letter in word:
+    if letter.lower() == 'a': a_count += 1
+    elif letter.lower() == 'e': e_count += 1
+    elif letter.lower() == 'i': i_count += 1
+    elif letter.lower() == 'o': o_count += 1
+    elif letter.lower() == 'u': u_count += 1
+    elif letter.lower() == 'y': y_count += 1
     if letter.lower() in vowels:
         vowel_count += 1
     elif letter.isalpha():
@@ -31,6 +43,8 @@ if vowel_count == 0 or consonant_count == 0:
     print("False")
 else:
     print(f"В слове имеется {vowel_count} гластных и {consonant_count} согласных.")
+    print("В слове имеется", a_count, "a,", e_count, "e,", i_count, "i,", o_count, "o,", u_count, "u, и", y_count, "y.")
+
 
 #Задание 3
 
